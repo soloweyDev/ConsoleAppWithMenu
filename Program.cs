028da542525ca96bd0328a864de7ca23fcf8f728
Dmitriy Anufriev <AnufrievDI@elcomplus.ru>
2021-09-25 11:49:56 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppWithMenu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
